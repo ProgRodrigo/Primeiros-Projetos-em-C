ce101cafea8eb9683876bdf0d8cc3f02301161b0
Rodrigo Vieira <rodrigovieirablack@gmail.com>
2019-11-26 09:38:47 -0300

    Cod em excesso retirado.

--- Média/main.c
+++ Média/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
  int  main(){
 float X,Y,Z,MD;
-        printf("\t\t Simulador!! \n\n\n");
+        printf("\t\t Calcule sua média! \n\n\n");
     printf("\n Digite a NP1: ");
     scanf("%f", &X);
     printf("\n Digite a NP2: ");
@@ -12,13 +12,10 @@ float X,Y,Z,MD;
     scanf("%f", &Z);
     MD = (4 * X + 4 * Y + 2 * Z) / 10;
         if (MD >= 5) {
-        printf("Voce esta Aprovado, Continue assim!!\n NP1:%.1f \t NP2:%.1f \t PIM:%.1f \t Media:%.1f \n",X,Y,Z, MD);
+        printf("Voce foi aprovado!!\n NP1:%.1f \t NP2:%.1f \t PIM:%.1f \t Media:%.1f \n",X,Y,Z, MD);
         }
          else{
-                if (MD < 5)
-        printf(" \n voce esta reprovado, Se esforce mais!! \n NP1:%.1f \t NP2:%.1f \t PIM:%.1f \t Media: %.1f \n ",X,Y,Z,MD);
-         else
-            printf(" \n Voce esta reprovado \t ");
+        printf(" \n voce foi reprovado, Se esforce mais!! \n NP1:%.1f \t NP2:%.1f \t PIM:%.1f \t Media: %.1f \n ",X,Y,Z,MD);
          }
     system(" \n \n pause");
 return 0;
